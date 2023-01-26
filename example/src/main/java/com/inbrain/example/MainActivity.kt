95a6e5ebecf5f5743712482db06139335087acf5
Sergey Blazhko <s.blazhko.dev@gmail.com>
2023-01-26 18:45:23 +0400

    - warning fixed; - kotlin version fixed

--- example/src/main/java/com/inbrain/example/MainActivity.kt
+++ example/src/main/java/com/inbrain/example/MainActivity.kt
@@ -69,7 +69,7 @@ class MainActivity : AppCompatActivity() {
 
         val chbSessionId = findViewById<CheckBox>(R.id.chbSessionId)
         val edtSessionId = findViewById<EditText>(R.id.edtSessionId)
-        chbSessionId.setOnCheckedChangeListener { compoundButton: CompoundButton?, checked: Boolean ->
+        chbSessionId.setOnCheckedChangeListener { _, checked: Boolean ->
             if (checked) {
                 val sessionId = edtSessionId.text.toString()
                 if (sessionId.isNotEmpty()) {
@@ -263,28 +263,28 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun applyUiCustomization() {
-        val toolBarConfig = ToolBarConfig()
-        toolBarConfig.title = getString(R.string.app_name) // set title
-        val useResourceId = false
-        if (useResourceId) {
-            toolBarConfig.setToolbarColorResId(R.color.colorAccent) // set toolbar color with status bar
-                .setBackButtonColorResId(R.color.white)
-                .titleColorResId = R.color.white //  set toolbar text
-        } else {
-            toolBarConfig.setToolbarColor(ContextCompat.getColor(this, R.color.colorAccent))
-                .setBackButtonColor(ContextCompat.getColor(this, R.color.white))
-                .titleColor = ContextCompat.getColor(this, R.color.white)
-        }
-        toolBarConfig.isElevationEnabled = false
-        InBrain.getInstance().setToolbarConfig(toolBarConfig)
-        val statusBarConfig = StatusBarConfig()
-        if (useResourceId) {
-            statusBarConfig.setStatusBarColorResId(R.color.white)
-                .setStatusBarIconsLight(false)
-        } else {
-            statusBarConfig.setStatusBarColor(ContextCompat.getColor(this, R.color.white))
-                .setStatusBarIconsLight(false)
-        }
-        InBrain.getInstance().setStatusBarConfig(statusBarConfig)
+//        val toolBarConfig = ToolBarConfig()
+//        toolBarConfig.title = getString(R.string.app_name) // set title
+//        val useResourceId = false
+//        if (useResourceId) {
+//            toolBarConfig.setToolbarColorResId(R.color.colorAccent) // set toolbar color with status bar
+//                .setBackButtonColorResId(R.color.white)
+//                .titleColorResId = R.color.white //  set toolbar text
+//        } else {
+//            toolBarConfig.setToolbarColor(ContextCompat.getColor(this, R.color.colorAccent))
+//                .setBackButtonColor(ContextCompat.getColor(this, R.color.white))
+//                .titleColor = ContextCompat.getColor(this, R.color.white)
+//        }
+//        toolBarConfig.isElevationEnabled = false
+//        InBrain.getInstance().setToolbarConfig(toolBarConfig)
+//        val statusBarConfig = StatusBarConfig()
+//        if (useResourceId) {
+//            statusBarConfig.setStatusBarColorResId(R.color.white)
+//                .setStatusBarIconsLight(false)
+//        } else {
+//            statusBarConfig.setStatusBarColor(ContextCompat.getColor(this, R.color.white))
+//                .setStatusBarIconsLight(false)
+//        }
+//        InBrain.getInstance().setStatusBarConfig(statusBarConfig)
     }
 }
