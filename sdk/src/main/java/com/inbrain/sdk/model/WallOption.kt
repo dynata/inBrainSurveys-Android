5eb53d403b34cd185a849a0553540833b154b529
Tiny @ngel <safe.nysocho305@outlook.com>
2024-12-04 09:08:53 +0100

    add fromRaw() to the WallOption enum

--- sdk/src/main/java/com/inbrain/sdk/model/WallOption.kt
+++ sdk/src/main/java/com/inbrain/sdk/model/WallOption.kt
@@ -1,7 +1,13 @@
 package com.inbrain.sdk.model
 
-enum class WallOption {
-    ALL,
-    SURVEYS,
-    OFFERS,
+enum class WallOption(val raw: Int) {
+    ALL(0),
+    SURVEYS(1),
+    OFFERS(2);
+
+    companion object {
+        fun fromRaw(raw: Int): WallOption? {
+            return values().find { it.raw == raw }
+        }
+    }
 }
